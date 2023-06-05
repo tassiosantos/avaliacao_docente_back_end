4df26c8113a3e9f14fe9df19fc3fbe811606e609
Joao Ferreira <joaovlima07@hotmail.com>
2023-06-05 15:34:47 -0300

    feat: discente and docentes services

---
+++ src/main/java/com/frameworks/sistemaavaliacao/secretaria/services/DocenteService.java
@@ -0,0 +1,41 @@
+package com.frameworks.sistemaavaliacao.secretaria.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.frameworks.sistemaavaliacao.secretaria.entities.Docente;
+import com.frameworks.sistemaavaliacao.secretaria.interfaces.DocenteRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class DocenteService {
+
+    private final DocenteRepository docenteRepository;
+
+    @Autowired
+    public DocenteService(DocenteRepository docenteRepository) {
+        this.docenteRepository = docenteRepository;
+    }
+
+    public Docente createDocente(Docente docente) {
+        return docenteRepository.save(docente);
+    }
+
+    public List<Docente> getAllDocentes() {
+        return docenteRepository.findAll();
+    }
+
+    public Optional<Docente> getDocenteById(Integer id) {
+        return docenteRepository.findById(id);
+    }
+
+    public Docente updateDocente(Docente docente) {
+        return docenteRepository.save(docente);
+    }
+
+    public void deleteDocente(Integer id) {
+        docenteRepository.deleteById(id);
+    }
+}
